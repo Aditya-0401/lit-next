cbff3cf885388e2b827922773f851bfdaf0ae4c7
Aditya Khandelwal <khandelwaladitya410@gmail.com>
2023-08-18 15:19:47 +0530

    Second Commit

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,13 @@ import Image from 'next/image'
 import React, { useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 
-
-
 export default function Home() {
   useEffect(()=>{
-    import("lit-newtry/bundle");
+    import("lit-aditya/dist/bundle");
   },[])
   return (
     <div>
-      Hii
+      Hii.
       <my-app></my-app>
     </div>
   )
